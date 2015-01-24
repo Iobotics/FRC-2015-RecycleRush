c15b9c5ea2efb81c7f051deaec479cf46b58f370
koluke <koluke@iolani.org>
2015-01-24 09:03:34 -1000

    Add Utility class for PowerScaler functionality

---
+++ src/org/iolani/frc/util/Utility.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iolani.frc.util;
+//import edu.wpi.first.wpilibj.*;
+//import edu.wpi.first.wpilibj.can.*;
+
+/**
+ *
+ * @author wkd
+ */
+public class Utility {
+    
+    public static double window(double input, double min, double max) {
+        if(input < min) return min;
+        if(input > max) return max;
+        return input;
+    }
+    
+    public static double window(double input, double max) {
+        return Utility.window(input, -max, max);
+    }
+    
+    public static double sign(double input) {
+        if(input == 0.0) return 0.0;
+        return (input > 0.0) ? 1.0 : -1.0; 
+    }
+}
