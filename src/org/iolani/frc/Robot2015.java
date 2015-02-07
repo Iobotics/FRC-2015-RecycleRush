ef0f14a3b98ab8488929e8d50ab7ab392acfbdfd
koluke <koluke@iolani.org>
2015-02-06 17:09:34 -1000

    Remove unnecessary code

--- src/org/iolani/frc/Robot2015.java
+++ src/org/iolani/frc/Robot2015.java
@@ -2,7 +2,6 @@
 package org.iolani.frc;
 
 import org.iolani.frc.commands.CommandBase;
-import com.kauailabs.nav6.frc.IMU;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 //import edu.wpi.first.wpilibj.command.Command;
@@ -30,7 +29,6 @@ public class Robot2015 extends IterativeRobot {
         // instantiate the command used for the autonomous period
         //autonomousCommand = new ExampleCommand();
 		CommandBase.init();
-		
     }
 	
 	public void disabledPeriodic() {
